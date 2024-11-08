3b716e35c36bef9b550f03f216999ce811486f51
Lobkov Constantine <klobkov@ozon.ru>
2024-11-09 00:48:38 +0300

    wip

--- src/scanner/error.rs
+++ src/scanner/error.rs
@@ -1,8 +1,7 @@
 #[derive(Debug, PartialEq)]
 pub enum ScannerErrorType {
     UnexpectedEndOfFile,
-    UnterminatedStartTag,
-    UnterminatedEndTag,
+    InvalidTagName,
 }
 
 #[derive(Debug)]
@@ -18,5 +17,9 @@ impl ScannerError {
 
     pub fn unexpected_end_of_file(line: usize) -> ScannerError {
         return ScannerError::new(ScannerErrorType::UnexpectedEndOfFile, line);
+    }
+
+    pub fn invalid_tag_name(line: usize) -> ScannerError {
+        return ScannerError::new(ScannerErrorType::InvalidTagName, line);
     }
 }
