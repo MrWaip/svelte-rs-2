c783c28d2184050d8175d0ecc2f1c607d575fbf8
Constantine Lobkov <37497663+MrWaip@users.noreply.github.com>
2024-11-27 22:26:38 +0300

    wip

--- src/ast/mod.rs
+++ src/ast/mod.rs
@@ -1,11 +1,32 @@
+pub struct Ast {
+    pub template: Vec<Node>,
+}
+
+pub trait FormatNode {
+    fn format_node(&self) -> String;
+}
+
 #[derive(Debug, PartialEq, Eq)]
 pub enum Node {
     Element(Element),
+}
+
+impl FormatNode for Node {
+    fn format_node(&self) -> String {
+        return match self {
+            Node::Element(element) => element.format_node(),
+        };
+    }
 }
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct Element {
     pub name: String,
     pub nodes: Vec<Box<Node>>,
-    
+}
+
+impl FormatNode for Element {
+    fn format_node(&self) -> String {
+        return "".to_string();
+    }
 }
---
+++ src/diagnostics/mod.rs
@@ -0,0 +1,45 @@
+#[derive(Debug, PartialEq)]
+pub enum DiagnosticType {
+    UnexpectedEndOfFile,
+    InvalidTagName,
+    UnterminatedStartTag,
+    InvalidAttributeName,
+    UnexpectedToken,
+    UnexpectedKeyword,
+}
+
+#[derive(Debug)]
+pub struct Diagnostic {
+    pub error_type: DiagnosticType,
+    pub line: usize,
+}
+
+impl Diagnostic {
+    pub fn new(error_type: DiagnosticType, line: usize) -> Self {
+        Diagnostic { error_type, line }
+    }
+
+    pub fn unexpected_end_of_file(line: usize) -> Diagnostic {
+        return Diagnostic::new(DiagnosticType::UnexpectedEndOfFile, line);
+    }
+
+    pub fn invalid_tag_name(line: usize) -> Diagnostic {
+        return Diagnostic::new(DiagnosticType::InvalidTagName, line);
+    }
+
+    pub fn unterminated_start_tag(line: usize) -> Diagnostic {
+        return Diagnostic::new(DiagnosticType::UnterminatedStartTag, line);
+    }
+
+    pub fn invalid_attribute_name(line: usize) -> Diagnostic {
+        return Diagnostic::new(DiagnosticType::UnterminatedStartTag, line);
+    }
+
+    pub fn unexpected_token(line: usize) -> Diagnostic {
+        return Diagnostic::new(DiagnosticType::UnexpectedToken, line);
+    }
+
+    pub fn unexpected_keyword(line: usize) -> Diagnostic {
+        return Diagnostic::new(DiagnosticType::UnexpectedKeyword, line);
+    }
+}
