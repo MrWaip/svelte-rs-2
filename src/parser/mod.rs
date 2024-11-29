a90bc9ce811f70c223feebfe7899f81969eb7d14
Lobkov Constantine <klobkov@ozon.ru>
2024-11-29 11:29:32 +0300

    wip

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,7 +1,9 @@
-use scanner::Scanner;
+use std::{mem, rc::Rc};
+
+use scanner::{token::StartTag, Scanner};
 
 use crate::{
-    ast::{Ast, Node},
+    ast::{Ast, AstNode, Element, Node},
     diagnostics::Diagnostic,
 };
 
@@ -10,7 +12,8 @@ pub mod scanner;
 pub struct Parser {
     scanner: Scanner,
     source: &'static str,
-    stack: Vec<Node>,
+    stack: Vec<Rc<Node>>,
+    roots: Vec<Rc<Node>>,
 }
 
 impl Parser {
@@ -21,6 +24,7 @@ impl Parser {
             scanner,
             source,
             stack: vec![],
+            roots: vec![],
         };
     }
 
@@ -28,17 +32,43 @@ impl Parser {
         for token in self.scanner.scan_tokens()?.iter() {
             match &token.r#type {
                 scanner::token::TokenType::Text => todo!(),
-                scanner::token::TokenType::StartTag(start_tag) => todo!(),
+                scanner::token::TokenType::StartTag(tag) => self.parse_start_tag(tag)?,
                 scanner::token::TokenType::EndTag => todo!(),
                 scanner::token::TokenType::Interpolation => todo!(),
-                scanner::token::TokenType::StartIfTag(start_if_tag) => todo!(),
-                scanner::token::TokenType::ElseTag(else_tag) => todo!(),
+                scanner::token::TokenType::StartIfTag(_start_if_tag) => todo!(),
+                scanner::token::TokenType::ElseTag(_else_tag) => todo!(),
                 scanner::token::TokenType::EndIfTag => todo!(),
                 scanner::token::TokenType::EOF => todo!(),
             }
         }
 
-        return Ok(Ast { template: vec![] });
+        let template = mem::replace(&mut self.roots, vec![]);
+
+        return Ok(Ast { template });
+    }
+
+    fn parse_start_tag(&mut self, tag: &StartTag) -> Result<(), Diagnostic> {
+        let name = tag.name.clone();
+        // let self_closing = tag.self_closing;
+
+        let node = Node::Element(Element {
+            name,
+            nodes: vec![],
+        });
+
+        let node = Rc::new(node);
+
+        if let Some(last) = self.stack.last_mut() {
+            last.push(node);
+        }
+
+        // self.stack.push(node.clone());
+
+        return Ok(());
+    }
+
+    fn parse_end_tag() -> Result<(), Diagnostic> {
+        Ok(())
     }
 }
 
