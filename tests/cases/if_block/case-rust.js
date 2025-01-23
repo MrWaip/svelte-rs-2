764e460953f78a4676432cd622df82e89c900c41
Constantine Lobkov <37497663+MrWaip@users.noreply.github.com>
2025-01-23 12:27:05 +0300

    wip

---
+++ tests/cases/if_block/case-rust.js
@@ -0,0 +1,24 @@
+import * as $ from "svelte/internal/client";
+var root_1 = $.template(`<div name="123"> </div><span>text</span>`, 1);
+var root = $.template(`<!><div></div>`, 1);
+export default function App($$anchor) {
+	let id = undefined;
+	var fragment = root();
+	var node = $.first_child(fragment);
+	{
+		var consequent = ($$anchor) => {
+			var fragment_1 = root_1();
+			var div = $.first_child(fragment_1);
+			var text = $.child(div, true);
+			$.reset(div);
+			$.next();
+			$.template_effect(() => $.set_text(text, id));
+			$.append($$anchor, fragment_1);
+		};
+		$.if(node, ($$render) => {
+			if (true) $$render(consequent);
+		});
+	}
+	$.next();
+	$.append($$anchor, fragment);
+}
---
+++ tests/cases/if_block/case.svelte
@@ -0,0 +1,3 @@
+<script>
+    let id = $state();
+</script>{#if true}<div name="123">{id}</div><span>text</span>{/if}<div></div>
