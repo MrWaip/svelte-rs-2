f95714a2f300a3c94db966dd818aa252b8e2a513
Lobkov Constantine <klobkov@ozon.ru>
2025-01-28 11:27:15 +0300

    update expression

--- tests/cases/mutation_in_template/case-rust.js
+++ tests/cases/mutation_in_template/case-rust.js
@@ -1,8 +1,8 @@
 import * as $ from "svelte/internal/client";
 var root = $.template(`<div>_</div> `, 1);
 export default function App($$anchor) {
-	let title = $.state(10);
-	let title2 = $.state(12);
+	let title = 10;
+	let title2 = 12;
 	var fragment = root();
 	var div = $.first_child(fragment);
 	var text = $.sibling(div, 1, true);
