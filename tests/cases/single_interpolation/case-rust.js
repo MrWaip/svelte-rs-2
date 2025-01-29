4e5bb5eecbde11070924272fc3020ea70eea9e6b
Lobkov Constantine <klobkov@ozon.ru>
2025-01-29 20:45:31 +0300

    single interpolation

--- tests/cases/single_interpolation/case-rust.js
+++ tests/cases/single_interpolation/case-rust.js
@@ -1,9 +1,8 @@
 import * as $ from "svelte/internal/client";
-var root = $.template(` `, 1);
 export default function App($$anchor) {
 	let name = undefined;
-	var fragment = root();
-	var text = $.first_child(fragment, true);
+	$.next();
+	var text = $.text();
 	$.template_effect(() => $.set_text(text, name));
-	$.append($$anchor, fragment);
+	$.append($$anchor, text);
 }
