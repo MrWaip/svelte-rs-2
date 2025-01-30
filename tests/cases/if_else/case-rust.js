360dab485210250db416f1e4c30d9ee246ae12a2
Lobkov Constantine <klobkov@ozon.ru>
2025-01-30 11:19:19 +0300

    wip

--- tests/cases/if_else/case-rust.js
+++ tests/cases/if_else/case-rust.js
@@ -1,5 +1,4 @@
 import * as $ from "svelte/internal/client";
-var root_1 = $.template(`<br> Text 1`, 1);
 var root_3 = $.template(`<br> Text 2`, 1);
 var root_4 = $.template(`<br> text 3`, 1);
 export default function App($$anchor) {
@@ -7,30 +6,29 @@ export default function App($$anchor) {
 	var node = $.first_child(fragment);
 	{
 		var consequent = ($$anchor) => {
-			var fragment_1 = root_1();
-			$.next();
-			$.append($$anchor, fragment_1);
+			var text = $.text("Text 1");
+			$.append($$anchor, text);
 		};
 		var alternate_1 = ($$anchor) => {
-			var fragment_2 = $.comment();
-			var node_1 = $.first_child(fragment_2);
+			var fragment_1 = $.comment();
+			var node_1 = $.first_child(fragment_1);
 			{
 				var consequent_1 = ($$anchor) => {
-					var fragment_3 = root_3();
+					var fragment_2 = root_3();
 					$.next();
-					$.append($$anchor, fragment_3);
+					$.append($$anchor, fragment_2);
 				};
 				var alternate = ($$anchor) => {
-					var fragment_4 = root_4();
+					var fragment_3 = root_4();
 					$.next();
-					$.append($$anchor, fragment_4);
+					$.append($$anchor, fragment_3);
 				};
 				$.if(node_1, ($$render) => {
 					if (1 + 2) $$render(consequent_1);
 else $$render(alternate, false);
 				}, true);
 			}
-			$.append($$anchor, fragment_2);
+			$.append($$anchor, fragment_1);
 		};
 		$.if(node, ($$render) => {
 			if (true) $$render(consequent);
