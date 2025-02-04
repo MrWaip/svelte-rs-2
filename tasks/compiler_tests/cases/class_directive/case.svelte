aa7e3933ababd1adacd910d431919497a7836c72
Lobkov Constantine <klobkov@ozon.ru>
2025-02-04 15:53:24 +0300

    static class directives

--- tasks/compiler_tests/cases/class_directive/case.svelte
+++ tasks/compiler_tests/cases/class_directive/case.svelte
@@ -5,10 +5,19 @@
     let unchanged = $state();
     let untouched = $state();
 
+    const staticClass = true;
+
     visible = 12;
     absolute = true;
 </script>
 
-<div class:visible class:absolute={absolute} class:unchanged class:untouched={untouched}>
+<div 
+    class:visible 
+    class:absolute={absolute} 
+    class:unchanged 
+    class:untouched={untouched}
+    class:staticClass
+    class:static2={true}
+>
     Lorem
 </div>
