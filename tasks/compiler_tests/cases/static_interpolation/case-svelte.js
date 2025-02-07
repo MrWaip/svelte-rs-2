dce741ebf09a2b7579d9442129c1734e8341d3f5
Lobkov Constantine <klobkov@ozon.ru>
2025-02-07 18:09:15 +0300

    tmp

--- tasks/compiler_tests/cases/static_interpolation/case-svelte.js
+++ tasks/compiler_tests/cases/static_interpolation/case-svelte.js
@@ -1,5 +1,5 @@
 import * as $ from "svelte/internal/client";
-var root = $.template(` <div></div>`, 1);
+var root = $.template(` <div><br> </div> <div></div>`, 1);
 export default function App($$anchor) {
 	const title = "world";
 	$.next();
@@ -7,6 +7,10 @@ export default function App($$anchor) {
 	var text = $.first_child(fragment);
 	text.nodeValue = `${title ?? ""} `;
 	var div = $.sibling(text);
-	div.textContent = title;
+	var text_1 = $.sibling($.child(div));
+	text_1.nodeValue = ` ${title ?? ""}`;
+	$.reset(div);
+	var div_1 = $.sibling(div, 2);
+	div_1.textContent = title;
 	$.append($$anchor, fragment);
 }
