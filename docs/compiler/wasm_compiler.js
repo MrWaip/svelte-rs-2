73bd0d911d9173c66a8b891135fa489e73038acf
Constantine Lobkov <37497663+MrWaip@users.noreply.github.com>
2025-03-23 18:12:51 +0300

    wip

--- docs/compiler/wasm_compiler.js
+++ docs/compiler/wasm_compiler.js
@@ -127,6 +127,24 @@ export class WasmCompiler {
             wasm.__wbindgen_free(deferred3_0, deferred3_1, 1);
         }
     }
+    /**
+     * @param {string} source
+     * @returns {string}
+     */
+    format(source) {
+        let deferred2_0;
+        let deferred2_1;
+        try {
+            const ptr0 = passStringToWasm0(source, wasm.__wbindgen_malloc, wasm.__wbindgen_realloc);
+            const len0 = WASM_VECTOR_LEN;
+            const ret = wasm.wasmcompiler_format(this.__wbg_ptr, ptr0, len0);
+            deferred2_0 = ret[0];
+            deferred2_1 = ret[1];
+            return getStringFromWasm0(ret[0], ret[1]);
+        } finally {
+            wasm.__wbindgen_free(deferred2_0, deferred2_1, 1);
+        }
+    }
 }
 
 async function __wbg_load(module, imports) {
