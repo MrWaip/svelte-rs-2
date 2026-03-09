dae6f7f234c7046ce7f1ebbab77a93a72725d5bf
Lobkov Constantine <klobkov@ozon.ru>
2026-03-09 22:27:58 +0300

    Stage 3d3

--- tasks/compiler_tests/cases2/generic_root_sequence/case-rust.js
+++ tasks/compiler_tests/cases2/generic_root_sequence/case-rust.js
@@ -10,23 +10,20 @@ export default function App($$anchor) {
 	var node = $.sibling(text_1, 2);
 	{
 		var consequent = ($$anchor) => {};
-		var alternate = ($$anchor) => {
-			var fragment_1 = $.comment();
-			var node_1 = $.first_child(fragment_1);
+		var alternate = ($$anchor, $$elseif) => {
 			{
 				var consequent_1 = ($$anchor) => {};
 				var alternate_1 = ($$anchor) => {};
-				$.if(node_1, ($$render) => {
+				$.if($$anchor, ($$render) => {
 					if (false) $$render(consequent_1);
 else $$render(alternate_1, false);
-				});
+				}, $$elseif);
 			}
-			$.append($$anchor, fragment_1);
 		};
 		$.if(node, ($$render) => {
 			if (true) $$render(consequent);
 else $$render(alternate, false);
-		}, true);
+		});
 	}
 	$.append($$anchor, fragment);
 }
