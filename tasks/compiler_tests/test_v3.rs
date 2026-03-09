59deeefe5988de636244503ae856827d01ab2bf7
Lobkov Constantine <klobkov@ozon.ru>
2026-03-09 21:27:01 +0300

    Stage 3c: test infra

--- tasks/compiler_tests/test_v3.rs
+++ tasks/compiler_tests/test_v3.rs
@@ -1,25 +1,84 @@
-use rstest::*;
 use std::{
     fs::{read_to_string, File},
     io::Write,
-    path::PathBuf,
+    path::Path,
 };
 
 use pretty_assertions::assert_eq;
+use rstest::rstest;
 use svelte_compiler::compile;
 
-#[rstest]
-fn integration(#[files("./cases2/**/*.svelte")] path: PathBuf) {
-    let file = read_to_string(&path).unwrap();
+fn assert_compiler(case: &str) {
+    let path = Path::new(env!("CARGO_MANIFEST_DIR"))
+        .join("cases2")
+        .join(case)
+        .join("case.svelte");
+    let input = read_to_string(&path).unwrap();
 
-    let actual = compile(&file).unwrap();
-    let path = path.parent().unwrap();
+    let actual = compile(&input).unwrap_or_else(|e| panic!("[{case}] compile error: {e:?}"));
 
-    let expected = read_to_string(path.join("case-svelte.js")).unwrap();
+    let dir = path.parent().unwrap();
+    let expected = read_to_string(dir.join("case-svelte.js")).unwrap();
 
-    let mut out = File::create(path.join("case-rust.js")).unwrap();
-
-    out.write_all(actual.js.as_bytes()).unwrap();
+    File::create(dir.join("case-rust.js"))
+        .unwrap()
+        .write_all(actual.js.as_bytes())
+        .unwrap();
 
     assert_eq!(actual.js, expected);
 }
+
+#[rstest]
+fn empty() { assert_compiler("empty"); }
+
+#[rstest]
+fn single_text_node() { assert_compiler("single_text_node"); }
+
+#[rstest]
+fn single_element() { assert_compiler("single_element"); }
+
+#[rstest]
+fn single_interpolation() { assert_compiler("single_interpolation"); }
+
+#[rstest]
+fn single_if_block() { assert_compiler("single_if_block"); }
+
+#[rstest]
+fn single_if_else_block() { assert_compiler("single_if_else_block"); }
+
+#[rstest]
+fn element_attributes() { assert_compiler("element_attributes"); }
+
+#[rstest]
+fn state_runes() { assert_compiler("state_runes"); }
+
+#[rstest]
+fn each_block() { assert_compiler("each_block"); }
+
+#[rstest]
+fn bind_directives() { assert_compiler("bind_directives"); }
+
+#[rstest]
+fn nested_elements() { assert_compiler("nested_elements"); }
+
+#[rstest]
+fn nested_resets() { assert_compiler("nested_resets"); }
+
+#[rstest]
+fn single_concatenation() { assert_compiler("single_concatenation"); }
+
+#[rstest]
+fn elements_childs() { assert_compiler("elements_childs"); }
+
+#[rstest]
+fn generic_root_sequence() { assert_compiler("generic_root_sequence"); }
+
+#[rstest]
+#[ignore] // spread attrs not supported yet
+fn spread_attribute() { assert_compiler("spread_attribute"); }
+
+#[rstest]
+fn utf8() { assert_compiler("utf8"); }
+
+#[rstest]
+fn smoke() { assert_compiler("smoke"); }
