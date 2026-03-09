b11f924d04f0f856f9685d96308b51fdf79568c6
Lobkov Constantine <klobkov@ozon.ru>
2026-03-09 21:08:35 +0300

    Stage 3b: test run

--- tasks/compiler_tests/cases2/each_block/case-rust.js
+++ tasks/compiler_tests/cases2/each_block/case-rust.js
@@ -1,5 +1,4 @@
 import * as $ from "svelte/internal/client";
-var root_1 = $.template(` <div> </div>`, 1);
 export default function App($$anchor) {
 	var fragment = $.comment();
 	var node = $.first_child(fragment);
@@ -8,16 +7,10 @@ export default function App($$anchor) {
 		2,
 		3
 	], $.index, ($$anchor, item) => {
-		$.next();
-		var fragment_1 = root_1();
+		var root = $.template(` <div></div>`);
+		var fragment_1 = root();
 		var text = $.first_child(fragment_1);
-		var div = $.sibling(text);
-		var text_1 = $.child(div);
-		$.reset(div);
-		$.template_effect(() => {
-			$.set_text(text, `${item ?? ""} `);
-			$.set_text(text_1, `${item ?? ""} + example`);
-		});
+		text.nodeValue = `${item ?? ""} `;
 		$.append($$anchor, fragment_1);
 	});
 	$.append($$anchor, fragment);
