fdf65fcd35852362a5f5c880d86a0e5c2cbc351c
Constantine Lobkov <37497663+MrWaip@users.noreply.github.com>
2026-03-11 23:41:54 +0300

    Fix simple test

--- tasks/compiler_tests/cases2/hello_state/case-rust.js
+++ tasks/compiler_tests/cases2/hello_state/case-rust.js
@@ -1,10 +1,8 @@
 import * as $ from "svelte/internal/client";
-var root = $.from_html(`<h1> </h1>`);
+var root = $.from_html(`<h1></h1>`);
 export default function App($$anchor) {
 	let name = "world";
 	var h1 = root();
-	var text = $.child(h1);
-	$.reset(h1);
-	$.template_effect(() => $.set_text(text, `Hello ${name ?? ""}!`));
+	h1.textContent = "Hello world!";
 	$.append($$anchor, h1);
 }
