c68cfeb85fe3ed9ba6c11c77a2c06a7d3afb7081
Constantine Lobkov <37497663+MrWaip@users.noreply.github.com>
2026-03-22 22:32:30 +0300

    fix test

--- tasks/compiler_tests/cases2/special_elements_all/case-rust.js
+++ tasks/compiler_tests/cases2/special_elements_all/case-rust.js
@@ -1,6 +1,6 @@
 import * as $ from "svelte/internal/client";
 var root_1 = $.from_html(`<meta name="viewport" content="width=device-width"/>`);
-var root = $.from_html(` <div><p> </p></div>`, 1);
+var root = $.from_html(`<div><p> </p></div>`);
 export default function App($$anchor) {
 	let count = $.state(0);
 	function handleEvent() {
@@ -9,7 +9,7 @@ export default function App($$anchor) {
 	function action(node) {
 		return { destroy() {} };
 	}
-	$.next();
+	var div = root();
 	$.head("q2w0q4", ($$anchor) => {
 		var meta = root_1();
 		$.deferred_template_effect(() => {
@@ -21,12 +21,10 @@ export default function App($$anchor) {
 	$.event("visibilitychange", $.document, handleEvent);
 	$.event("mouseenter", $.document.body, handleEvent);
 	$.action($.document.body, ($$node) => action?.($$node));
-	var fragment = root();
-	var div = $.sibling($.first_child(fragment));
 	var p = $.child(div);
 	var text = $.child(p);
 	$.reset(p);
 	$.reset(div);
 	$.template_effect(() => $.set_text(text, `Count: ${$.get(count) ?? ""}`));
-	$.append($$anchor, fragment);
+	$.append($$anchor, div);
 }
