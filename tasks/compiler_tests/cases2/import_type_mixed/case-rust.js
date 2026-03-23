a2dd652ea792880211ef9e4c9c895ea3c80f7efb
Lobkov Constantine <klobkov@ozon.ru>
2026-03-23 21:29:37 +0300

    tests

--- tasks/compiler_tests/cases2/import_type_mixed/case-rust.js
+++ tasks/compiler_tests/cases2/import_type_mixed/case-rust.js
@@ -1,14 +1,18 @@
 import * as $ from "svelte/internal/client";
 import { realValue } from "./utils";
-var root = $.from_html(`<button></button>`);
-export default function App($$anchor) {
+var root = $.from_html(`<button> </button>`);
+export default function App($$anchor, $$props) {
+	$.push($$props, true);
 	let data = $.proxy({ value: 0 });
 	function process(input) {
 		return realValue.transform(input);
 	}
 	var button = root();
-	button.textContent = realValue.label;
+	var text = $.child(button, true);
+	$.reset(button);
+	$.template_effect(() => $.set_text(text, realValue.label));
 	$.delegated("click", button, () => process(data));
 	$.append($$anchor, button);
+	$.pop();
 }
 $.delegate(["click"]);
