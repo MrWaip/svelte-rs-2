c1eba4c41b7b0ded29e70ad4dc752ca1b44ce0ed
Lobkov Constantine <klobkov@ozon.ru>
2026-04-04 13:28:12 +0300

    Add script module passthrough coverage

---
+++ tasks/compiler_tests/cases2/script_module_export_specifiers/case.svelte
@@ -0,0 +1,10 @@
+<script module>
+	const foo = 'foo';
+	function bar() {
+		return foo.toUpperCase();
+	}
+
+	export { foo, bar };
+</script>
+
+<p>module exports</p>
---
+++ tasks/compiler_tests/cases2/script_module_imports/case.svelte
@@ -0,0 +1,11 @@
+<script module>
+	import { writable } from 'svelte/store';
+
+	const theme = writable('light');
+</script>
+
+<script>
+	let count = $state(0);
+</script>
+
+<p>{count}</p>
