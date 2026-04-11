0ea02ed659087f7d7db76913ddb0a8d61b590c98
Constantine Lobkov <45konstantin2@gmail.com>
2026-04-11 16:52:16 +0300

    Add ignored diagnose case for input shorthand prop lowering mismatch (#183)

--- tasks/compiler_tests/test_v3.rs
+++ tasks/compiler_tests/test_v3.rs
@@ -2774,3 +2774,9 @@ fn snippet_destructure_default_state_ref() {
 fn snippet_destructure_default_mutated_state_ref() {
     assert_compiler("snippet_destructure_default_mutated_state_ref");
 }
+
+#[rstest]
+#[ignore = "diagnose: pending fix"]
+fn diagnose_props_bindable_icon_component() {
+    assert_compiler("diagnose_props_bindable_icon_component");
+}
