35078e22dd05eee7da9c10ac606536b63e874710
Constantine Lobkov <37497663+MrWaip@users.noreply.github.com>
2026-04-11 16:52:16 +0300

    Add ignored diagnose case for input shorthand prop lowering mismatch (#183)

---
+++ tasks/compiler_tests/cases2/diagnose_props_bindable_icon_component/case-rust.js
@@ -0,0 +1,33 @@
+import * as $ from "svelte/internal/client";
+var root_1 = $.from_html(`<div class="ui-input-icon svelte-nbptzh"><!></div>`);
+var root = $.from_html(`<div class="ui-input-wrapper svelte-nbptzh"><!> <input class="ui-input svelte-nbptzh"/></div>`);
+export default function App($$anchor, $$props) {
+	$.push($$props, true);
+	let value = $.prop($$props, "value", 11, ""), placeholder = $.prop($$props, "placeholder", 3, ""), type = $.prop($$props, "type", 3, "text"), disabled = $.prop($$props, "disabled", 3, false);
+	var div = root();
+	var node = $.child(div);
+	{
+		var consequent = ($$anchor) => {
+			var div_1 = root_1();
+			var node_1 = $.child(div_1);
+			$.component(node_1, () => $$props.icon, ($$anchor, Icon_1) => {
+				Icon_1($$anchor, {});
+			});
+			$.reset(div_1);
+			$.append($$anchor, div_1);
+		};
+		$.if(node, ($$render) => {
+			if ($$props.icon) $$render(consequent);
+		});
+	}
+	var input = $.sibling(node, 2);
+	$.reset(div);
+	$.template_effect(() => {
+		$.set_attribute(input, "value", value());
+		$.set_attribute(input, "placeholder", placeholder());
+		$.set_attribute(input, "type", type());
+		$.set_attribute(input, "disabled", disabled());
+	});
+	$.append($$anchor, div);
+	$.pop();
+}
